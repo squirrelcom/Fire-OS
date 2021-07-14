26ae560d768b9cf1fec1460240610ade97150308
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-14 09:07:05 +0100

    Update LogoASCII.cs

--- OSProject/LogoASCII.cs
+++ OSProject/LogoASCII.cs
@@ -9,12 +9,15 @@ namespace OSProject
         public static void main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine(" ______   _____   _____    ______        ____     _____ ");
-            Console.WriteLine("|  ____| |_   _| |  __ \\  |  ____|      / __ \\   / ____|");
-            Console.WriteLine("| |__      | |   | |__) | | |__        | |  | | | (___  ");
-            Console.WriteLine("|  __|     | |   |  _  /  |  __|       | |  | |  \\___ \\ ");
-            Console.WriteLine("| |       _| |_  | | \\\\   | |____      | |__| |  ____) |");
-            Console.WriteLine("|_|      |_____| |_|  \\\\  |______|      \\____/  |_____/ ");
+            Console.WriteLine(" .d8888b.   .d88888b.  888     888 8888888 8888888b.  8888888b.  8888888 Y88b   d88P");
+            Console.WriteLine("d88P  Y88b d88P" "Y88b 888     888   888   888   Y88b 888   Y88b   888    Y88b d88P");
+            Console.WriteLine("Y88b.      888     888 888     888   888   888    888 888    888   888     Y88o88P");
+            Console.WriteLine("  Y888b.   888     888 888     888   888   888   d88P 888   d88P   888      Y888P");
+            Console.WriteLine("     Y88b. 888     888 888     888   888   8888888P   8888888P     888      d888b");
+            Console.WriteLine("       888 888 Y8b 888 888     888   888   888 T88b   888 T88b     888     d88888b");
+            Console.WriteLine("Y88b  d88P Y88b.Y8b88P Y88b. .d88P   888   888  T88b  888  T88b    888    d88P Y88b");
+            Console.WriteLine(" Y8888P      Y888888     Y88888P   8888888 888   T88b 888   T88b 8888888 d88P   Y88b ");
+            Console.WriteLine("                  Y8b");
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
