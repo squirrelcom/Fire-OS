8311ccf1413c5ba58b2b40c1b5d34a46d1b40723
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-15 09:45:45 +0100

    Update About.cs

--- OSProject/About.cs
+++ OSProject/About.cs
@@ -11,8 +11,6 @@ namespace OSProject
             Console.WriteLine("\nVersion 1.0.0\n");
             Console.WriteLine("Created & Copyrighted by");
             Console.WriteLine("Squirrel Computers ");
-            Console.WriteLine("18BCE1044\t\t\t18BCE1307\n");
-            Console.WriteLine("Vellore Institute of Technology, Chennai.");
             if(Console.ReadKey().Key == ConsoleKey.Enter)
                     return;
         }
