082fa28b84cee2907f06beb761e1c64051bbc511
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-16 08:07:34 +0100

    Update LogoASCII.cs

--- OSProject/LogoASCII.cs
+++ OSProject/LogoASCII.cs
@@ -8,16 +8,13 @@ namespace OSProject
     {
         public static void main()
         {
-            Console.ForegroundColor = ConsoleColor.DeepSkyBlue;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(" .d8888b.   .d88888b.  888     888 8888888 8888888b.  8888888b.  8888888 Y88b   d88P");
             Console.WriteLine("d88P  Y88b d88P" "Y88b 888     888   888   888   Y88b 888   Y88b   888    Y88b d88P");
-            Console.ForegroundColor = ConsoleColor.Moccasin;
             Console.WriteLine("Y88b.      888     888 888     888   888   888    888 888    888   888     Y88o88P");
             Console.WriteLine("  Y888b.   888     888 888     888   888   888   d88P 888   d88P   888      Y888P");
-            Console.ForegroundColor = ConsoleColor.MediumVioletRed;
             Console.WriteLine("     Y88b. 888     888 888     888   888   8888888P   8888888P     888      d888b");
             Console.WriteLine("       888 888 Y8b 888 888     888   888   888 T88b   888 T88b     888     d88888b");
-            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Y88b  d88P Y88b.Y8b88P Y88b. .d88P   888   888  T88b  888  T88b    888    d88P Y88b");
             Console.WriteLine(" Y8888P      Y888888     Y88888P   8888888 888   T88b 888   T88b 8888888 d88P   Y88b ");
             Console.WriteLine("                  Y8b");
