0a03b4f21c5ef4bd21f961b66595830e557d956b
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-16 08:38:18 +0100

    Update LogoS.cs

--- OSProject/LogoS.cs
+++ OSProject/LogoS.cs
@@ -16,7 +16,7 @@ namespace OSProject
             Console.WriteLine("     Y88b. 888     888 888     888   888   8888888P   8888888P     888      d888b         888d88888b888    d88P  888 8888888P   888");
             Console.WriteLine("       888 888 Y8b 888 888     888   888   888 T88b   888 T88b     888     d88888b        88888P Y88888   d88P   888 888 T88b   888");
             Console.WriteLine("Y88b  d88P Y88b.Y8b88P Y88b. .d88P   888   888  T88b  888  T88b    888    d88P Y88b       8888P   Y8888  d8888888888 888  T88b  888");
-            Console.WriteLine(" Y8888P      Y888888     Y88888P   8888888 888   T88b 888   T88b 8888888 d88P   Y88b      888P     Y888 d88P     888 888   T88b 8888888888");
+            Console.WriteLine(" Y8888P      Y888888     Y88888P   8888888 888   T88b 888   T88b 8888888 d88P   Y88b      888P     Y888 d88P     888 888   T88b 8888888888  Launcher 1.0.0");
             Console.WriteLine("                  Y8b");
             Console.ForegroundColor = ConsoleColor.White;
         }
