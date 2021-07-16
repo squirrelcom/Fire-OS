e159aa28cbd309f83cae50fd3d7323638bf7c6d3
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-16 08:36:22 +0100

    Create Squirrixwarelogo.cs

---
+++ OSProject/Squirrixwarelogo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSProject
+{
+    class LogoS
+    {
+        public static void main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" .d8888b.   .d88888b.  888     888 8888888 8888888b.  8888888b.  8888888 Y88b   d88P      888       888        d8888 8888888b.  8888888888");
+            Console.WriteLine("d88P  Y88b d88P" "Y88b 888     888   888   888   Y88b 888   Y88b   888    Y88b d88P       888   o   888       d88888 888   Y88b 888");
+            Console.WriteLine("Y88b.      888     888 888     888   888   888    888 888    888   888     Y88o88P        888  d8b  888      d88P888 888    888 888");
+            Console.WriteLine("  Y888b.   888     888 888     888   888   888   d88P 888   d88P   888      Y888P         888 d888b 888     d88P 888 888   d88P 8888888");
+            Console.WriteLine("     Y88b. 888     888 888     888   888   8888888P   8888888P     888      d888b         888d88888b888    d88P  888 8888888P   888");
+            Console.WriteLine("       888 888 Y8b 888 888     888   888   888 T88b   888 T88b     888     d88888b        88888P Y88888   d88P   888 888 T88b   888");
+            Console.WriteLine("Y88b  d88P Y88b.Y8b88P Y88b. .d88P   888   888  T88b  888  T88b    888    d88P Y88b       8888P   Y8888  d8888888888 888  T88b  888");
+            Console.WriteLine(" Y8888P      Y888888     Y88888P   8888888 888   T88b 888   T88b 8888888 d88P   Y88b      888P     Y888 d88P     888 888   T88b 8888888888");
+            Console.WriteLine("                  Y8b");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
